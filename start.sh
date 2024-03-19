c687fb982dbcb56f4a63a71df95d945e28f22602
Dᴀʀᴋ_Aɴɢᴇʟ <84849690+COLD-ONEZ@users.noreply.github.com>
2024-03-19 06:50:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/COLD-ONEZ/Cx-Rqst-Filter.git /Cx-Rqst-Filter
+  git clone https://github.com/COLD-ONEZ/Cx-Series-BoT.git /Cx-Series-BoT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Cx-Rqst-Filter
+  git clone $UPSTREAM_REPO /Cx-Series-BoT
 fi
-cd /Cx-Rqst-Filter
+cd /Cx-Series-BoT
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
