2e38dd007239debc59ddd55fb102d8b3e5640fb1
Dᴀʀᴋ_Aɴɢᴇʟ <84849690+COLD-ONEZ@users.noreply.github.com>
2024-03-25 23:26:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,6 +19,10 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
+async def delete_after_delay(message: Message, delay):
+    await asyncio.sleep(AUTO_DELETE_TIME)
+    await message.delete()
+
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
@@ -137,21 +141,16 @@ async def start(client, message):
             if f_caption is None:
                 f_caption = f"{title}"
             try:
-                fsm = await client.send_cached_media(
+                v = await message.reply_text(f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>{AUTO_DELETE} minutes</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</b>")
+                m = await client.send_cached_media(
                     chat_id=message.from_user.id,
                     file_id=msg.get("file_id"),
                     caption=f_caption,
                     protect_content=msg.get('protect', False),
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                         [
-                          InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=f'https://t.me/TEAM_COLD')
-                       ],[
-                          InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="https://t.me/TEAM_COLD_BOT")
-                         ]
-                        ]
-                    )
                 )
+
+                asyncio.create_task(delete_after_delay(k, AUTO_DELETE_TIME))
+                asyncio.create_task(delete_after_delay(h, AUTO_DELETE_TIME))
             except FloodWait as e:
                 await asyncio.sleep(e.x)
                 logger.warning(f"Floodwait of {e.x} sec.")
