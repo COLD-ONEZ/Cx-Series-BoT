c6ebb413547abd04a1e7ebfbc6fa1f4e3143d8c4
Muhammedshifin <muhammedshifink4@gmail.com>
2024-04-07 21:24:28 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -8,7 +8,7 @@ from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 from database.join_reqs import JoinReqs
-from info import REQ_CHANNEL, AUTH_CHANNEL, JOIN_REQS_DB, ADMINS
+from info import REQ_CHANNEL, AUTH_CHANNEL, JOIN_REQS_DB, ADMINS, CUSTOM_FILE_CAPTION
 from utils import check_loop_sub, get_size
 from logging import getLogger
 from database.ia_filterdb import get_file_details
