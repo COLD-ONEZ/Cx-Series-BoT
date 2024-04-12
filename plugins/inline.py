7a6962ad07f572c235487ec8d7851f6f7fe38079
Dᴀʀᴋ_Aɴɢᴇʟ <84849690+COLD-ONEZ@users.noreply.github.com>
2024-04-12 08:38:28 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -35,7 +35,7 @@ async def answer(bot, query):
     if (AUTH_CHANNEL or REQ_CHANNEL) and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='❗Click Here And Join Our Channel❗',
                            switch_pm_parameter="subscribe")
         return
 
